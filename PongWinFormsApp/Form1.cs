ec532344546cc3171031b2a04eff136d0e2a514c
SimonasBan <sbansevicius@gmail.com>
2021-09-27 10:03:24 +0300

    Add forms project

---
+++ PongWinFormsApp/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PongWinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            var form2 = new Form1();
+            form2.Show();
+            //Application.Run(new Form1());
+        }
+    }
+}
