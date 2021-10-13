01f1ba7fb0bf8d8ed4c49e134a584c472088fb71
Mr-Farty <59486349+Mr-Farty@users.noreply.github.com>
2021-10-13 17:58:32 +0300

    Hope nothing fucked up.

---
+++ PingPong3/Patterns/AbstractFactory/ExplodePowerUpFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPong3.Patterns.AbstractFactory
+{
+    public class ExplodePowerUpFactory : PowerUpFactory
+    {
+        public PUEffect addPUEffect()
+        {
+            return new PUPowerUpEffect();
+        }
+    }
+}
