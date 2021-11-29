9ad61401b82d57f9b322dd66cb705e4bb1ca4c80
SimonasBan <sbansevicius@gmail.com>
2021-11-29 09:58:05 +0200

    Add first test

---
+++ PingPong3Tests/Patterns/Command/BallResetCommandTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using PingPong3.Patterns.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPong3.Patterns.Command.Tests
+{
+    public class BallResetCommandTests
+    {
+        [Fact()]
+        public void UndoTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
