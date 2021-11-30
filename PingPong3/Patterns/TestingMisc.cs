876213510860f1dfa712891dd90b601e41da5da3
SimonasBan <sbansevicius@gmail.com>
2021-11-30 14:51:39 +0200

    add

---
+++ PingPong3/Patterns/TestingMisc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPong3.Patterns
+{
+    public class TestingMisc
+    {
+        public int GenerateBallVelocityX(int seed)
+        {
+            if (seed < 0)
+            {
+                return -1;
+            }
+            else if (seed < 2)
+            {
+                return 2;
+            }
+            return 0;
+        }
+    }
+}
