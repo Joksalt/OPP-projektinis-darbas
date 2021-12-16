a1c7d79fd3db82e24a7f5a545d491335efc21451
Joksalt <60965298+Joksalt@users.noreply.github.com>
2021-12-16 20:03:38 +0200

    Flyweight pattern
    Implemented flyweight pattern for powerUp images. If an image was already used then it returns its value. Otherwise it creates a new image and stores it for later use(if needed)

---
+++ PingPong3/Patterns/Flyweight/ImageFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPong3.Patterns.Flyweight
+{
+    public class ImageFactory
+    {
+        public Dictionary<string, string> cache = new Dictionary<string, string>();
+
+        public string GetImagePath(string key)
+        {
+            if (cache.ContainsKey(key))
+                return cache[key];
+
+            String path = System.IO.Directory.GetCurrentDirectory();
+            path = path.Substring(0, path.LastIndexOf("bin\\Debug"));
+            path += "Images\\";
+            path = path + key + ".png";
+            cache.Add(key, path);
+            return path;
+        }
+
+    }
+}
